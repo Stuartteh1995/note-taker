57b70b3ce50dd378687154b9d61a438cc74ae7f9
stuart teh <stuartteh1995@gmail.com>
2023-05-08 23:24:42 +1000

    update to server.js to make the application work. Had to redownload the starter code to debug it

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -10,50 +11,55 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// HTML routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 // API routes
 app.get('/api/notes', (req, res) => {
-  // Read db.json and return all saved notes as JSON
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
 });
 
 app.post('/api/notes', (req, res) => {
-  // Receive a new note, add it to db.json, and return the new note
+  const newNote = req.body;
+  newNote.id = uuidv4();
+
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json(newNote);
+    });
+  });
 });
 
-// HTML routes
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const updatedNotes = notes.filter(note => note.id !== noteId);
+
+    fs.writeFile('db/db.json', JSON.stringify(updatedNotes), (err) => {
+      if (err) throw err;
+      res.json({ message: 'Note deleted' });
+    });
+  });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
-
-
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      const notes = JSON.parse(data);
-      res.json(notes);
-    });
-  });
-  
-  app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuidv4();
-  
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      const notes = JSON.parse(data);
-      notes.push(newNote);
-  
-      fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-        if (err) throw err;
-        res.json(newNote);
-      });
-    });
-  });
-  
